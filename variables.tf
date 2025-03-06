bdc83c119549e787d8ed49413b6d08f3dffb12c3
SanjayHoysal <127748051+SanjayHoysal@users.noreply.github.com>
2025-03-06 15:42:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,9 @@
-variable "region" {
-  description = "The AWS region to deploy resources"
-  default     = "us-east-1"
-}
-
-variable "cidr_block" {
-  description = "The CIDR block for the VPC"
-}
-
-variable "name" {
-  description = "The name for the resources"
-}
-
-variable "ami" {
-  description = "The AMI to use for the EC2 instance"
-}
-
-variable "instance_type" {
-  description = "The type of EC2 instance"
-  default     = "t2.micro"
-}
-
-variable "availability_zone" {
-  description = "The availability zone to deploy the subnet"
-  default     = "us-east-1a"  # Change this to your desired availability zone
+module "vpc_ec2" {
+  source        = "github.com/SanjayHoysal/vpc-ec2-module/modules/vpc-ec2"
+  region        = var.region
+  cidr_block    = var.cidr_block
+  name          = var.name
+  ami           = var.ami
+  instance_type = var.instance_type
+  availability_zone = var.availability_zone
 }
